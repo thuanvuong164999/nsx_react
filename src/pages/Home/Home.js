2fd8163e06949778a908b886cd2470a93343c644
thuanvuong164999 <dungthuan2468@gmail.com>
2019-05-31 20:57:38 +0700

    biult apps

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,16 +2,49 @@ import React from  'react'
 import './Home.css'
 
 class HomePage extends React.Component {
+    // tao danh sach data
+    constructor(){
+        super() 
+        // cau lenh bat buot trong constructor
+        this.state = {
+            data: [
+                {
+                    name:'Thuan'
+                },
+                {
+                    name:'Son'
+                },
+                {
+                    name:'Mai'
+                },
+                {
+                    name:'An'
+                }
+            ]
+        }
+        console.log(this.state.data)
+        // show list tren console
+    }
+    // show data trong HTML
     render(){
         return(
             <React.Fragment>
-            <div className ='home-page-top'>
-                Hello
-            </div>
-            <div>
-                Home Page
+            <div className="page-home">
+                <ul>
+                    {
+                        this.state.data.map((item, index) => {
+                            return(
+                            <li>
+                                <span className="item-title">Name:</span>
+                                <span className="item-name">{item.name}</span>
+                            </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
             </React.Fragment>
+            // tag co the chua nhieu div trong React
         )
     }
 }
