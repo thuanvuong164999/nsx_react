0483bf24cf0f4063b11aa92b3503e87d102ef2d0
thuanvuong164999 <dungthuan2468@gmail.com>
2019-06-03 21:22:56 +0700

    biuld apps

--- src/pages/Products/detail/detail.js
+++ src/pages/Products/detail/detail.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './detail.scss'
 
 class DetailPage extends React.Component{
+    constructor(props){
+        super(props)
+        console.log(this.props)
+        const {match: {params}} = this.props
+        console.log(params, params.id)
+        //vào console coi
+    }
     render(){
         return(
             <React.Fragment>
