14ee9621be6c1af015d76cfa22828528b4ce895d
thuanvuong164999 <thuanvuong164999>
2019-06-07 20:22:12 +0700

    react client

---
+++ client/src/pages/login/login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class LoginPage extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+            <div className ='login-page-top'>
+                Hello
+            </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default LoginPage
--- client/src/routers.js
+++ client/src/routers.js
@@ -4,15 +4,25 @@ import HomePage from './pages/Home/Home'
 import ProductsPage from './pages/Products/Products'
 import FarmsPage from './pages/farms/farms'
 import DetailPage from './pages/Products/detail/detail'
+import Auth from './components/auth-component/auth-component'
+import LoginPage from './pages/login/login'
 
 const MainRouter = () => (
     <main>
         <Switch>
+        <Route exact path='/' render={
+            () => (
+                <Auth orRedirectTo='/login' orRender={
+                    <HomePage></HomePage>
+                }></Auth>
+            )
+        }></Route>
             //tao api
             <Route exact path='/' component={HomePage}></Route>
             <Route path='/Products' component={ProductsPage}></Route>
             <Route path='/Farms' component={FarmsPage}></Route>
             <Route path='/product/:id' component={DetailPage}></Route>
+            <Route path='/login' component={LoginPage}></Route>
         </Switch>
     </main>
 )
