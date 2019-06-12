1f40f74475416de1be438c13f4d7d126a746e75a
thuanvuong164999 <dungthuan2468@gmail.com>
2019-06-12 19:39:26 +0700

    chat_page

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const port = 5000
+const http = require('http')
+// http được install sẳn vời nodejs
+const socketID = require('socket.io')
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const server = http.createServer(app)
+// khởi tạo 1 server thông qua http
+
+server.listen(port, () => {
+    console.log(`Server started with port. ${port}`)
+    // sử dụng dấu `` (quy ước theo javascript) thay cho ''
+    // có thể sử dụng cộng chuỗi
+    // ('Server started with port.' + port)
+    // dấu () là viết tắt của function() : 
+    // comback thông tin khi port hk sử dụng được
+})
+
